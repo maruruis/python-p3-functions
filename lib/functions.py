2ab6345e2cf6e1bcaf0db2cc1ee8ff482bc15fda
Ian Kimani <ianmwng@gmail.com>
2023-11-20 14:48:31 +0300

    complete assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default(name=None):
+    if name is None:
+        print("Hello, programmer!")
+    else:
+        print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(a, b):
+    return a + b
 
-def halve(number):
-    pass
+def halve(n):
+    return n / 2
